899344fadf36fabaf3ca2bb2565bba769f3c7f20
Dmitry Osobov <waffencode@gmail.com>
2024-04-04 13:26:30 +0300

    Add docs & extend constructor

--- University.Test/Domain/UserTest.cs
+++ University.Test/Domain/UserTest.cs
@@ -14,8 +14,10 @@ public class UserTest
     [Fact]
     public void ShouldInstantiateUserWithConstructorWithParameters()
     {
-        var user = new User("User", "example@example.com", "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b");
+        var id = Guid.NewGuid();
+        var user = new User(id, "User", "example@example.com", "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b");
         Assert.NotNull(user);
+        Assert.Equal(id, user.Id);
         Assert.Equal("User", user.Username);
         Assert.Equal("example@example.com", user.Email);
         Assert.Equal("6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b", user.PasswordHash);
@@ -50,5 +52,13 @@ public class UserTest
     {
         var user = new User();
         Assert.NotEqual(Guid.Empty, user.Id);
+    }
+
+    [Fact]
+    public void ShouldInitializeIdOnObjectInstantiation()
+    {
+        var id = Guid.NewGuid();
+        var user = new User() { Id = id };
+        Assert.Equal(id, user.Id);
     }
 }
