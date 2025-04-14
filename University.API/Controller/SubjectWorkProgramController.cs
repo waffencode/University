844d0ae5213e8cc2d001a9c5c9eb43871c78dc4c
Dmitry Osobov <waffencode@gmail.com>
2025-04-14 13:42:53 +0300

    refactor: improve docs of subjectworkprogram delete method

--- University.API/Controller/SubjectWorkProgramController.cs
+++ University.API/Controller/SubjectWorkProgramController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using University.Domain;
+using University.Exceptions;
 using University.Infrastructure;
 using University.Repository;
 
@@ -52,11 +54,39 @@ public class SubjectWorkProgramController : ControllerBase
         return Ok();
     }
 
+    /// <summary>
+    /// Asynchronously deletes a <see cref="SubjectWorkProgram"/> entity with the specified ID.
+    /// </summary>
+    /// <param name="id">The GUID of the <see cref="SubjectWorkProgram"/> entity to delete.</param>
+    /// <param name="cancellationToken">Cancellation token to cancel deletion.</param>
+    /// <response code="200">The <see cref="SubjectWorkProgram"/> was successfully deleted.</response>
+    /// <response code="404">Entity with the specified ID not found in database.</response>
+    /// <response code="503">The operation was canceled or the service is unavailable.</response>
+    /// <response code="400">The request is invalid.</response>
     [HttpDelete("{id:guid}")]
     [Authorize]
-    public async Task<ActionResult> Delete(Guid id)
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status503ServiceUnavailable)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
     {
-        await _repository.DeleteAsync(id);
-        return Ok();
+        try
+        {
+            await _repository.DeleteAsync(id, cancellationToken);
+            return Ok();
+        }
+        catch (EntityNotFoundException exception)
+        {
+            return NotFound(exception.Message);
+        }
+        catch (OperationCanceledException exception)
+        {
+            return StatusCode(StatusCodes.Status503ServiceUnavailable, exception.Message);
+        }
+        catch (Exception exception)
+        {
+            return BadRequest(exception.Message);
+        }
     }
 }
