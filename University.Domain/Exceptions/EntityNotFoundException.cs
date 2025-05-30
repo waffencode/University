188a8abd5fa7de60728f767305376fd2ca9a9f24
Dmitry Osobov <waffencode@gmail.com>
2025-05-30 17:06:16 +0300

    docs: add documentation for EntityNotFoundException

--- University.Domain/Exceptions/EntityNotFoundException.cs
+++ University.Domain/Exceptions/EntityNotFoundException.cs
@@ -1,21 +1,30 @@
 namespace University.Exceptions;
 
+/// <summary>
+/// Represents an exception which is thrown when an entity is not found in the database.
+/// </summary>
+/// <author>waffencode@gmail.com</author>
 public class EntityNotFoundException: Exception
 {
-    public EntityNotFoundException()
-    {
-    }
-
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EntityNotFoundException"/> with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public EntityNotFoundException(string? message) : base(message)
     {
     }
-    
-    public EntityNotFoundException(Type type, string id) 
-        : base($"{type.Name} with the ID {id} was not found in the database.")
-    {
-    }
 
-    public EntityNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EntityNotFoundException"/> with a specific <see cref="Type"/> and an ID.
+    /// </summary>
+    /// <param name="type">The <see cref="Type"/> of the entity which was not found.</param>
+    /// <param name="id">The ID of the entity which was not found.</param>
+    /// <example>This code: <code>throw new EntityNotFoundException(typeof(User), Guid.Empty.ToString())</code>
+    /// will throw this exception with the following message:
+    /// <code>User with the ID 00000000-0000-0000-0000-000000000000 was not found in the database.</code>
+    /// </example>
+    public EntityNotFoundException(Type type, string id)
+        : base($"The {type.Name} with the ID {id} was not found in the database.")
     {
     }
 }
