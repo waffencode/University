0009f559a2d4bbbd0d5dd0bbafeef22ddf43754a
Dmitry Osobov <waffencode@gmail.com>
2025-09-14 09:17:28 +0300

    docs: add comments in ApiSecurityExtensions class

--- University.API/Security/ApiSecurityExtensions.cs
+++ University.API/Security/ApiSecurityExtensions.cs
@@ -5,31 +5,43 @@ using University.Domain;
 
 namespace University.Security;
 
+/// <summary>
+/// Provides extension methods for <see cref="IServiceCollection"/> to add authentication and authorization.
+/// </summary>
 public static class ApiSecurityExtensions
 {
+    /// <summary>
+    /// Adds authentication and authorization to specified services collection.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/> to add authentication and authorization on.</param>
+    /// <param name="configuration"><see cref="IConfiguration"/> to get JwtOptions from.</param>
     public static void AddApiAuthentication(this IServiceCollection services, IConfiguration configuration)
     {
         var jwtOptions = configuration.GetSection(nameof(JwtOptions)).Get<JwtOptions>();
 
         if (jwtOptions is not null)
         {
+            // Using JWT for authentication.
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
                 {
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuer = false,
-                        ValidateAudience  = false,
-                        ValidateLifetime  = true,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
                         ValidateIssuerSigningKey = true,
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SecretKey)),
                     };
-
+                    
+                    // Define OnMessageReceived event during JWT validation.
                     options.Events = new JwtBearerEvents
                     {
                         OnMessageReceived = context =>
                         {
+                            // Pass the token from cookie to the message context.
                             context.Token = context.Request.Cookies["token"];
+                            // Continue processing.
                             return Task.CompletedTask;
                         }
                     };
@@ -43,6 +55,10 @@ public static class ApiSecurityExtensions
         }
     }
 
+    /// <summary>
+    /// Defines authorization policies for specified services collection.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/> to define policies on.</param>
     public static void AddApiAuthorization(this IServiceCollection services)
     {
         services.AddAuthorizationBuilder()
